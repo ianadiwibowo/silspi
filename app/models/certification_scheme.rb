959616b2dc987cffbe1cae810176e6c5f9802d0e
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2017-10-26 23:59:35 +0700

    Add validations for CompetencyUnit and CertificationScheme

--- app/models/certification_scheme.rb
+++ app/models/certification_scheme.rb
@@ -1,5 +1,9 @@
 class CertificationScheme < ApplicationRecord
   has_many :competency_units
 
-  enum type: %i[occupation cluster]
+  enum type: { occupation: 0, cluster: 1 }
+
+  validates :code, :name, :scheme_type, :certification_fee, :description, presence: true
+  validates :code, :name, uniqueness: true
+  validates :certification_fee, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/competency_unit.rb
+++ app/models/competency_unit.rb
@@ -1,3 +1,6 @@
 class CompetencyUnit < ApplicationRecord
   belongs_to :certification_scheme
+
+  validates :code, :name, :display_order, :certification_scheme, presence: true
+  validates :code, uniqueness: true
 end
