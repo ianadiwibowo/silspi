9504e9a165c7299332e8766161f64142146b80fe
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2017-11-04 19:11:39 +0700

    Make CompetencyUnit as CertificationScheme children in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :competency_units
-  resources :certification_schemes
+  resources :certification_schemes do
+    resources :competency_units
+  end
   get '/reset', to: 'development#reset'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
